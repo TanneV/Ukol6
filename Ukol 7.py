35ee5b81f54c8e40e82458700c7eccb48414d730
Tatiana Vajsová <98206827+TanneV@users.noreply.github.com>
2023-02-26 20:55:51 +0100

    Add files via upload

---
+++ Ukol 7.py
@@ -0,0 +1,27 @@
+class Auto:
+    def __init__(self, registracni_znacka, typ_vozidla, najete_km):
+        self.registracni_znacka = registracni_znacka
+        self.typ_vozidla = typ_vozidla
+        self.najete_km = najete_km
+        self.dostupne = True
+
+auto_1 = auto_1(Registracni_znacka = "4A2 3020", typ_vozidla = "Peugeot 403 Cabrio", najete_km = 47534, dostupne = True)
+auto_2 = auto_2(Registracni_znacka = "1P3 4747", typ_vozidla = "Skoda Octavia", najete_km = 41253, dostupne = True)
+
+def pujc_auto(self):
+        self.dostupne = False
+
+if Auto (dostupne = True): 
+    print(f"Potvrzuji zapujceni vozidla")
+else:
+    print(f"Vozidlo neni k dispozici")
+
+def __str__(self):
+    return f"Registracni znacka: {self.registracni_znacka}, typ vozidla: {self.typ_vozidla}, najete_km: {self.najete_km} km."
+
+input(f"Jakou znacku si prejete?")
+input(Auto)
+
+print(Auto)
+Auto.get_info()
+Auto.pujc_auto()
